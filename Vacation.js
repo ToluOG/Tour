5cbc3deab45323d0b1f26a9672451af85d8b0387
ToluOG <64303485+ToluOG@users.noreply.github.com>
2022-03-17 14:06:41 -0600

    initial commit

---
+++ Vacation.js
@@ -0,0 +1,36 @@
+
+// document.getElementById("Demo").addEventListener("mouseover", mouseOver);
+// document.getElementById("Demo").addEventListener("mouseout", mouseOut);
+// function Demo() {
+//     var x = document.getElementById("rectangle3");
+//     if (x.style.display === "none") {
+//      x.style.display = "block";
+//     } else {
+//      x.style.display = "none";
+//     }
+//    }
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("demo").addEventListener("mouseover", mouseOver);
+// document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+// function mouseOver() {
+//     document.getElementById("demo").style.color = "red";
+//   }
+  
+//   function mouseOut() {
+//     document.getElementById("demo").style.color = "black";
+//   }
